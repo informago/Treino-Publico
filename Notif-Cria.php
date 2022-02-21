ab865fb8a9da18593a360b19c31ea00575d7bdfa
Luciano Reis <luciano@larsoft.com.br>
2022-02-21 16:40:53 -0300

    Cria Notificação 02

--- Notif-Cria.php
+++ Notif-Cria.php
@@ -1,38 +1,40 @@
 ﻿<html>
 <head>
 </head>
+<body>
 
 <?php
 
 if( isset($_POST['Usuario'])) {
-	Notifica($_POST['Tipo'], $_POST['Usuario'], $_POST[Apontamento] );
+
+	$Usuario = $_POST['Usuario'];
+	$Apontamento = 0;
+	$Tipo = "A";
+	$Mensagem = $_POST['Mensagem'];
+
+	$cnNotif = new mysqli( "localhost", "lucianor_t01", "treino-0102030405", "lucianor_t01-notif");
+	$cmNotif = "SELECT Id FROM Notificacoes WHERE Usuario = '" . $Usuario . "' AND Apontamento = " . $Apontamento . " AND Tipo ='" . $Tipo . "';";
+	$res = $cnNotif->query( $cmNotif );
+
+	echo "Número de Linhas: " . strval($res->num_rows) . "<br />";
+	
+	if( $res->num_rows > 0 ){
+		// Atualiza notificação existente
+		$cmNotif = "UPDATE Notificacoes SET Lido = 0, DataHora = NOW() WHERE Usuario = '" . $Usuario . "' AND Apontamento = " . $Apontamento . " AND Tipo = '" . $Tipo . "';";
+		$cnNotif->query( $cmNotif );
+		echo "Atualizou <br />";
+	}
+	else {
+		// Insere nova notificação
+		$cmNotif = "INSERT INTO Notificacoes( Tipo, Usuario, Apontamento, Mensagem, Lido, DataHora ) VALUES( '" . $Tipo . "','" . $Usuario . "'," . $Apontamento . ", $Mensagem ,0,NOW() );";
+		$cnNotif->query( $cmNotif );
+		echo "Incluiu <br />";
+	}
+	$cnNotif->close();
 }
-
-function Notifca( $Tipo, $Usuario, $Apontamento ) {   
-
- $cnNotif = new mysqli( "localhost", "lucianor_t01", "treino-0102030405", "lucianor_t01-notif");   
-   
- $cmNotif = "SELECT Id FROM Notificacoes WHERE Usuario = '" . $Usuario . "' AND Apontamento = " . $Apontamento . " AND Tipo ='" . $Tipo . "';";   
- $res = $cnNotif->query( $cmNotif );   
-    
- // Existe notificação   
- if( $res->num_rows > 0 ){   
-  	// Atualiza notificação existente   
-  	$cmNotif = "UPDATE Notificacoes SET Lido = 0, DataHora = NOW() WHERE Usuario = '" . $Usuario . "' AND Apontamento = " . $Apontamento . " AND Tipo = '" . $Tipo . "';";   
-  	$cnNotif->query( $cmNotif );   
- }   
- else{  
-  	// Insere nova notificação  
-  	$cmNotif = "INSERT INTO `notification`( `type`, `forUser`, `entityID` ) VALUES( '" . $type . "','" . $forUser . "'," . $entityID . " );";   
-  	$cnNotif->query( $cmNotif );   
- }  
- $cnNotif->close();  
-} 
 
 ?>
 
-</head>
-<body>
 <form action="<?php echo str_replace('%7E','~',$_SERVER['REQUEST_URI']); ?>" method="post" name="Notifica">
 <table>
 	<tr>
@@ -43,6 +45,9 @@ function Notifca( $Tipo, $Usuario, $Apontamento ) {
 	</tr>
 	<tr>
 		<td>Apontamento</td><td><input type='text' name='Apontamento'/></td>
+	</tr>
+	<tr>
+		<td>Mensagem</td><td><input type='text' name='Mensagem'/></td>
 	</tr>
 	<tr>
 		<td>&nbsp;</td><td><input type='submit' name='Envia' value='Envia Notificação' /></td>
--- PreparaBD.sql
+++ PreparaBD.sql
@@ -1,18 +1,18 @@
 ﻿CREATE TABLE Usuarios (
 	Id Int PRIMARY KEY AUTO_INCREMENT,
 	Apelido VarChar(50),
-	Nome VarChar(50),
+	Nome VarChar(50)
 );
 
-CREATE TABLE Notificacoes (   
- Id Int PRIMARY KEY AUTO_INCREMENT,   
- Tipo VarChar(10),   
- Usuario VarChar(50),
- Mensagem VarChar(200),
- Apontamento Int,   
- Lido TinyInt(1) DEFAULT 0,   
- DataHora DateTime DEFAULT Now(),   
- FOREIGN KEY( Usuario ) REFERENCES Usuarios( Apelido )   
+CREATE TABLE Notificacoes (
+	Id Int PRIMARY KEY AUTO_INCREMENT,
+	Tipo VarChar(10),
+	Usuario VarChar(50),
+	Mensagem VarChar(200),
+	Apontamento Int,
+	Lido TinyInt(1) DEFAULT 0,
+	DataHora DateTime DEFAULT Now(),
+	FOREIGN KEY(Usuario) REFERENCES Usuarios(Apelido)   
 );   
 
 INSERT INTO Usuarios (Apelido, Nome) VALUES
