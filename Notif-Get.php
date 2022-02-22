7d25bcfeabe191c097fb2d1123140da575438020
Luciano Reis <luciano@larsoft.com.br>
2022-02-22 11:12:59 -0300

    Get Notif

--- Notif-Get.php
+++ Notif-Get.php
@@ -1,5 +1,4 @@
-﻿<!DOCTYPE html>
-
+﻿
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
@@ -9,79 +8,73 @@
 
 <?php
 
-$con = new mysqli( "localhost", "root", "password", "testDB");
+$cnNotif = new mysqli( "localhost", "lucianor_t01", "treino-0102030405", "lucianor_t01-notif");
 
-$reset = (int) $_GET[ "reset" ]; // either 1 or 0 ( true and false )
-$username = "user1"; // the user who's notifications we will be loading
+$reset = (int) $_GET[ "reset" ]; // 1 ou 0 para true ou false
+$username = "informago";
 
 if( $reset === 1 ) { 
-    $sql = "SELECT * FROM `notification` WHERE `forUser`='" . $username . "' ORDER BY `time` DESC LIMIT 10;";
-    setcookie( "loadedNotifications", "10", time() + 86400, "/" ); // store the cookie holding the amount of loaded notifications
+    $sql = "SELECT * FROM Notificacoes WHERE Usuario = '" . $username . "' ORDER BY DataHora DESC LIMIT 10;";
+    setcookie( "qtNotifCarregadas", "10", time() + 86400, "/" );
 }
 else {
-    $loadedNots=(int) $_COOKIE[ "loadedNotifications" ]; // get the amount of previously loaded notifications
-    $sql = "SELECT * FROM `notification` WHERE `forUser`='" . $username . "' ORDER BY `time` DESC LIMIT " . $loadedNots . " 10;";
-    $loadedNots = (string)( $loadedNots + 10 ); // calculate how many notifications have been loaded after query
-    setcookie( "loadedNotifications", $loadedNots, time() + 86400, "/" ); // update cookie with new value
+    $qtNotif = (int) $_COOKIE[ "qtNotifCarregadas" ];
+    $sql = "SELECT * FROM Notificacoes WHERE Usuario = '" . $username . "' ORDER BY DataHora DESC LIMIT " . $qtNotif . ", 10;";
+    $qtNotif = (string)( $qtNotif + 10 );
+    setcookie( "qtNotifCarregadas", $qtNotif, time() + 86400, "/" );
 }
 
-$result = $con->query( $sql );
-$notifications = array(); // declare an array to store the fetched notifications
+$result = $cnNotif->query( $sql );
+$aNotifs = array();
+// Monta array de resposta
 
 if( $result->num_rows > 0 ){
     while( $row = $result->fetch_assoc() ){
-        $notifications[] = array( "id" => $row[ "notificationID" ], "type" => $row[ "type" ], "entityID" => $row[ "entityID" ], "read" => $row[ "read" ], "text" => "" );
+        $aNotifs[] = array( "id" => $row[ "Id" ], "tipo" => $row[ "Tipo" ], "apontamento" => $row[ "Apontamento" ], "lido" => $row[ "Lido" ], "texto" => "" );
     }
 }
 else {
-    // no more notifications
+    // Sem mais notificações
 }
 
-/*
-* now we need to find the activity that relates to the notification
-* and create a text message that will be displayed to the user
-* containing the users who are responsible for that particular activity
-*/
+for( $i = 0; $i < count( $aNotifs ); $i++ ){
+    $sql = "";
 
-for( $i = 0; $i < count( $notifications ); $i++ ){
-    $sql = ""; // reset query string each time loop runs
-
-    // use different code for each type of notification ( ie. comments or ratings )
-    switch( $notifications[ $i ][ "type" ] ){
-    case "comment":
-        $sql = "SELECT `author` FROM `comment` WHERE `postID`=" . $notifications[ $i ][ "entityID" ] . ";";
-        $result = $con->query( $sql );
-        /*
-        * For this example we want a maximum of two names in the notification text
-        * if there are more than 2, then we'll include those as a number
-        */
+    switch( $aNotifs[ $i ][ "tipo" ] ){
+    case "A":
+        $sql = "SELECT Autor FROM Comentarios WHERE Post =" . $aNotifs[ $i ][ "apontamento" ] . ";";
+        $result = $cnNotif->query( $sql );
         
+        // Se houver mais de duas linhas na resposta, ela será substituída pelo número
         if( $result->num_rows === 1 ){
             $row = $result->fetch_assoc();
-            $name = $row[ "author" ];
-            $notifications[ $i ][ "text" ] = $name . " commented on your post";
+            $Nome = $row[ "Autor" ];
+            $aNotifs[ $i ][ "texto" ] = $Nome . " comentou no seu post.";
         }
         elseif( $result->num_rows === 2 ){
             $row = $result->fetch_assoc();
-            $name1 = $row[ "author" ]; // fetch first name
+            $Nome1 = $row[ "Autor" ];
             $row = $result->fetch_assoc();
-            $name2 = $row[ "author" ]; // fetch second name
-            $notifications[ $i ][ "text" ] = $name1 . " and " . $name2 . " commented on your post";
+            $Nome2 = $row[ "Autor" ];
+            $aNotifs[ $i ][ "texto" ] = $Nome1 . " e " . $Nome2 . " comentaram na sua postagem.";
         }
         elseif( $result->num_rows > 2 ){
-            $total = $result->num_rows - 2 //fetch the number of users who commented minus the two names we will use
+            $total = $result->num_rows - 2;
             $row = $result->fetch_assoc();
-            $name1 = $row[ "author" ]; // fetch first name
+            $Nome1 = $row[ "Autor" ];
             $row = $result->fetch_assoc();
-            $name2 = $row[ "author" ]; // fetch second name
-            $notifications[ $i ][ "text" ] = $name1 . ", " . $name2 . " and " . $total . " others commented on your post";
+            $Nome2 = $row[ "Autor" ];
+            $aNotifs[ $i ][ "texto" ] = $Nome1 . ", " . $Nome2 . " e outras " . $total . " comentaram no seu post.";
         }
         break;
-    // other cases to suit your needs
+    case "B":
+        break;
+    case "C":
+        break;
     }
 }
 
-echo( json_encode( $notifications ) ); // convert array to JSON text  
+echo( json_encode( $aNotifs ) );
 ?>
 
 </body>
--- PreparaBD.sql
+++ PreparaBD.sql
@@ -1,21 +1,42 @@
 ﻿CREATE TABLE Usuarios (
 	Id Int PRIMARY KEY AUTO_INCREMENT,
-	Apelido VarChar(50),
-	Nome VarChar(50)
+	Apelido VarChar(50) NOT NULL DEFAULT '',
+	Nome VarChar(50) NOT NULL DEFAULT ''
 );
 
 CREATE TABLE Notificacoes (
 	Id Int PRIMARY KEY AUTO_INCREMENT,
-	Tipo VarChar(10),
-	Usuario VarChar(50),
-	Mensagem VarChar(200),
-	Apontamento Int,
-	Lido TinyInt(1) DEFAULT 0,
-	DataHora DateTime DEFAULT Now(),
-	FOREIGN KEY(Usuario) REFERENCES Usuarios(Apelido)   
+	Tipo VarChar(10) NOT NULL DEFAULT '',
+	Usuario VarChar(50) NOT NULL DEFAULT '',
+	Mensagem VarChar(200) NOT NULL DEFAULT '',
+	Apontamento Int NOT NULL DEFAULT 0,
+	Lido TinyInt(1) NOT NULL DEFAULT 0,
+	DataHora DateTime NOT NULL DEFAULT Now(),
+	FOREIGN KEY(Usuario) REFERENCES Usuarios(Apelido)
 );   
+
+CREATE TABLE Postagens (
+	Id Int PRIMARY KEY AUTO_INCREMENT,
+	Texto VarChar(250) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE Comentarios (
+	Id Int PRIMARY KEY AUTO_INCREMENT,
+	Post Int,
+	Autor VarChar(100) NOT NULL DEFAULT '',
+	Texto VarChar(250) NOT NULL DEFAULT ''
+);
 
 INSERT INTO Usuarios (Apelido, Nome) VALUES
 	('informago','Luciano Reis'),
 	('upezao','Gabriel Reis'),
-	('dida','Myllena Reis')
+	('dida','Myllena Reis');
+
+INSERT INTO Postagens (Texto) VALUES
+	('Exemplo 01'),
+	('Exemplo 02');
+
+INSERT INTO Comentarios (Post, Autor, Texto) VALUES
+	( 1, 'Pessoa A','Gostei do artigo'),
+	( 1, 'Pessoa B','Podia melhorar'),
+	( 2, 'Pessoa A','Já desse eu não gostei');
